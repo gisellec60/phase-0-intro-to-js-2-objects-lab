cd798ee98d93352b8d4a80cb4c10bb0b87413c35
Giselle Smith <gisellec60@gmail.com>
2023-03-01 20:51:42 -0500

    complete lab for Obj

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "Giselle Carson",
+    streetAdress: "2032 Muirfield Village Way"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj, [key]:value}
+}
+ 
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+ 
+function deleteFromEmployeeByKey(obj, key) {
+    const newobj = { ...obj }
+    delete newobj[key];
+    return newobj;
+}
+ 
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+ }
